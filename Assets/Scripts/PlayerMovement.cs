78c24e9ad7a44dc00baad1b67ce36823491a8a9c
weixiangbeak <73181874+weixiangbeak@users.noreply.github.com>
2021-07-06 15:10:40 +0800

    Able to collect items using Raycasting
    Waiting for actual objects to be tested on

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public GameObject[] uiOnClose;
+
     public CharacterController controller;
 
     public Camera fpsCam;
+
+    public float damage = 10f;
 
     public float speed = 12f;
     public float gravity = -9.81f;
     public float jumpHeight = 3f;
     public float interactionDistance = 10f;
+    public float collectInteractionDistance = 0.5f;
 
     Vector3 velocity;
     bool isGrounded;
@@ -19,6 +24,13 @@ public class PlayerMovement : MonoBehaviour
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
+
+    void Start()
+    {
+        uiOnClose = GameObject.FindGameObjectsWithTag("ShowOnClose");
+        hideClose();
+        
+    }
 
     // Update is called once per frame
     void Update()
@@ -46,12 +58,67 @@ public class PlayerMovement : MonoBehaviour
 
         controller.Move(velocity * Time.deltaTime);
 
-        RaycastHit result;
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
 
+        Collect();
+    }
+
+    void Shoot()
+    {
+        RaycastHit result;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out result, interactionDistance))
         {
             Debug.Log(result.transform.name);
+
+            Target target = result.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+    }
+
+    void Collect()
+    {
+        RaycastHit result;
+        Debug.DrawLine(fpsCam.transform.position, fpsCam.transform.position + fpsCam.transform.forward * collectInteractionDistance);
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out result, collectInteractionDistance))
+        {
+            if (result.transform.name == "Collectible")
+            {
+                showClose();
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    result.transform.gameObject.SetActive(false);
+                    hideClose();
+                }
+            }
+
+            else
+            {
+                hideClose();
+            }
         }
 
     }
+
+    void hideClose()
+    {
+        foreach (GameObject s in uiOnClose)
+        {
+            s.SetActive(false);
+        }
+    }
+
+    void showClose()
+    {
+        foreach (GameObject s in uiOnClose)
+        {
+            s.SetActive(true);
+        }
+    }
+
 }
