bbab780b1833117d15ed45f78deb016b9360843f
Christian-Alexander-BB <S10205535@connect.np.edu.sg>
2021-07-17 15:04:38 +0800

    Adjusted door interaction
    Adjusted door interaction

--- Assets/Scripts/DoorInteractions.cs
+++ Assets/Scripts/DoorInteractions.cs
@@ -8,16 +8,27 @@ public class DoorInteractions : MonoBehaviour
     public Camera playerCam;
     public float maxRayDistanceToVShopDoor = 2f;
     public GameObject blackScreenImage;
-    public string exitPrompt = "Press F to leave";
-    public GameObject[] allText;
+    public string exitVinylShopPrompt = "Press F to leave";
+    public Text[] allText;
     
     [SerializeField]
     private RaycastHit hitObject;
 
+    [SerializeField]
+    private int framesPassedSinceRayHit = 0;
+
+    [SerializeField]
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach(Text text in allText)
+        {
+            if(text.gameObject.name == "ExitVinylShopPrompt")
+            {
+                text.text = exitVinylShopPrompt;
+            }
+        }
     }
 
     // Update is called once per frame
@@ -27,7 +38,13 @@ public class DoorInteractions : MonoBehaviour
             hitObject.transform.gameObject.name == "VinylShopDoor")
         {
             Debug.Log("1 thing");
-            StartCoroutine("ActivateExitVinylShopPrompt");
+            framesPassedSinceRayHit += 1;
+
+            if (framesPassedSinceRayHit == 1)
+            {
+                StartCoroutine("ActivateExitVinylShopPrompt");
+            }
+
             Debug.Log("2 things");
             
             if (Input.GetKeyDown(KeyCode.F))
@@ -35,25 +52,39 @@ public class DoorInteractions : MonoBehaviour
                 blackScreenImage.SetActive(true);
             }
         }
+
+        else
+        {
+            framesPassedSinceRayHit = 0;
+        }
     }
 
     IEnumerator ActivateExitVinylShopPrompt()
     {
-        foreach (GameObject text in allText)
+        for(int i = 0; i < allText.Length; ++i)
         {
             Debug.Log("Text");
-            if (text.name == "ExitVinylShopPrompt")
+            if (allText[i].gameObject.name != "ExitVinylShopPrompt")
             {
-                text.SetActive(true);
+                allText[i].color = new Color32(0, 0, 0, 0);
             }
 
             else
             {
-                Debug.Log("Close This");
-                text.SetActive(false);
+                allText[i].color = new Color32(50, 50, 50, 255);
             }
         }
 
-        yield return new WaitForSeconds(1f);
+        yield return null;
+    }
+
+    IEnumerator DeactivateExitVinylShopPrompt()
+    {
+        for(int i = 0; i < allText.Length; ++i)
+        {
+
+        }
+
+        yield return null;
     }
 }
