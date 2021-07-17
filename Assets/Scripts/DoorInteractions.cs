a07cd599df8e5660d7047ed61d83c9da4c2b2f4d
weixiangbeak <73181874+weixiangbeak@users.noreply.github.com>
2021-07-17 16:37:29 +0800

    testing3
    testing3

--- Assets/Scripts/DoorInteractions.cs
+++ Assets/Scripts/DoorInteractions.cs
@@ -5,46 +5,57 @@ using UnityEngine.UI;
 
 public class DoorInteractions : MonoBehaviour
 {
+    //Public variables
     public Camera playerCam;
     public float maxRayDistanceToVShopDoor = 2f;
-    public GameObject blackScreenImage;
+    public Image blackScreenImage;
     public Text[] allText;
-    
-    [SerializeField]
+    public Color defaultColor = new Color32(50, 50, 50, 255);
+    public float fadeAmount = 0.1f;
+
+    //Private variables
     private RaycastHit hitObject;
-
-    [SerializeField]
     private int framesPassedSinceRayHit = 0;
-
-    [SerializeField]
     private int framesPassedWithoutRayHit = 0;
+    private bool insideBuilding = true;
+    private string insideBuildingPrompt = "Press E To Leave";
+    private string outsideBuildingPrompt = "Press E To Enter";
+    private Color invisible = new Color32(0, 0, 0, 0);
+    private byte opacity = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //opacity = (byte)(blackScreenImage.color.a);
     }
 
     // Update is called once per frame
     void Update()
     {
         if(Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hitObject, maxRayDistanceToVShopDoor) && 
-            hitObject.transform.gameObject.name == "VinylShopDoor")
+            hitObject.transform.gameObject.tag == "Door")
         {
-            Debug.Log("1 thing");
             framesPassedSinceRayHit += 1;
             framesPassedWithoutRayHit = 0;
 
             if (framesPassedSinceRayHit == 1)
             {
-                StartCoroutine("ActivateExitVinylShopPrompt");
+                StartCoroutine("ActivateExitOrEnterPrompt");
             }
-
-            Debug.Log("2 things");
             
-            if (Input.GetKeyDown(KeyCode.F))
+            if (Input.GetKeyDown(KeyCode.E))
             {
-                blackScreenImage.SetActive(true);
+                StartCoroutine("FadeToBlack");
+
+                if (insideBuilding)
+                {
+                    insideBuilding = false;
+                }
+
+                else
+                {
+                    insideBuilding = true;
+                }
             }
         }
 
@@ -55,44 +66,68 @@ public class DoorInteractions : MonoBehaviour
 
             if (framesPassedWithoutRayHit == 1)
             {
-                StartCoroutine("DeactivateExitVinylShopPrompt");
+                StartCoroutine("DeactivateExitOrEnterPrompts");
             }
 
         }
     }
 
-    IEnumerator ActivateExitVinylShopPrompt()
+    IEnumerator ActivateExitOrEnterPrompt()
     {
         for(int i = 0; i < allText.Length; ++i)
         {
-            Debug.Log("Text");
-            if (allText[i].gameObject.name != "ExitVinylShopPrompt")
+            if (allText[i].gameObject.name == "Prompt")
             {
-                allText[i].color = new Color32(0, 0, 0, 0);
+                if (insideBuilding)
+                {
+                    allText[i].text = insideBuildingPrompt;
+                }
+
+                else
+                {
+                    allText[i].text = outsideBuildingPrompt;
+                }
+
+                allText[i].color = defaultColor;
             }
 
             else
             {
-                allText[i].color = new Color32(50, 50, 50, 255);
+                allText[i].color = invisible;
+
             }
         }
 
         yield return null;
     }
 
-    IEnumerator DeactivateExitVinylShopPrompt()
+    IEnumerator DeactivateExitOrEnterPrompts()
     {
         for(int i = 0; i < allText.Length; ++i)
         {
-            if (allText[i].gameObject.name == "ExitVinylShopPrompt")
+            if (allText[i].gameObject.name == "Prompt")
             {
-                allText[i].color = new Color32(0, 0, 0, 0);
+                allText[i].color = invisible;
             }
 
             else
             {
-                allText[i].color = new Color32(50, 50, 50, 255);
+                allText[i].color = defaultColor;
             }
+        }
+
+        yield return null;
+    }
+
+    IEnumerator FadeToBlack()
+    {
+        while (opacity < 255)
+        {
+            opacity += (byte)(fadeAmount);
+            blackScreenImage.color = new Color32((byte)(blackScreenImage.color.r * 255),
+                                                 (byte)(blackScreenImage.color.g * 255),
+                                                 (byte)(blackScreenImage.color.b * 255),
+                                                 opacity);
         }
 
         yield return null;
